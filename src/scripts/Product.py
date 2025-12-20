77c8f84bb319090a94f2369d8f9485e0b58fe7d5
Emil Henningsen <tzs820@alumni.ku.dk>
2025-12-20 17:34:07 +0100

    completed day 2

--- src/scripts/Product.py
+++ src/scripts/Product.py
@@ -7,6 +7,7 @@ class Product:
 
     _invalid = False
     _number = 0
+    _numberStr = ""
     _len = 0
     _leftPart = ""
     _rightPart = ""
@@ -14,6 +15,8 @@ class Product:
     partTwo = True
 
     def __init__(self, number : str):
+        self._numberStr = number
+        self._number = int(number)
         length = len(number)
         self._len = length
         #no need to check uneven lengths, as they are always valid product numbers
@@ -26,7 +29,7 @@ class Product:
         if self.partTwo:
             self.checkProductNumberPartTwo()
 
-        self._number = int(number)
+        
         
     def isInvalid(self) -> bool:
         return self._invalid
@@ -45,15 +48,20 @@ class Product:
     def checkProductNumberPartTwo(self):
         #dividers to check (i.e. is number length 6, then dividers are 1, 2 and 3)
         dividers = [divider for divider in range(1, self._len) if self._len % divider == 0]
-        print(dividers)
+        #print(dividers)
         #check if number is invalid with any of the found dividers
-        numberString = str(self._number)
-        print(numberString)
+        numberString = self._numberStr
+        #print(numberString)
         for divider in dividers:
             parts = []
-            for i in range(divider):
+            for i in range(int(self._len/divider)):
                 part = numberString[i*divider:(i+1)*divider]
-                print(part)
+                #print(part)
                 parts.append(part)
             #check if all parts are the same
-            self.setInvalid(all(part == parts[0] for part in parts))
+            isInvalid = all([part == parts[0] for part in parts])
+            self.setInvalid(isInvalid)
+            #print(isInvalid)
+            if isInvalid:
+                break
+            
