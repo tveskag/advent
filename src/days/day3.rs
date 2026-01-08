0a7adc28d81727f6e3bc41a62282caad27054bf7
Asger Viggo <Asgervkn@gmail.com>
2026-01-08 05:14:18 +0100

    Day 3, the easy way

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -1,39 +1,22 @@
 pub fn run(content: &str) -> usize {
-    let length = 2;
-    content
-        .lines()
-        .fold((0, length), &find_joltage)
-        .0
-}
+    let mut counter = 0;
+    let length = 12;
 
-fn find_joltage(
-    (counter, length): (usize, usize),
-    pack: &str,
-) -> (usize, usize) {
-    (
-        counter
-            + (0..length)
-                .rev()
-                .fold((0, pack.len(), pack), &count_voltage)
-                .0,
-        length,
-    )
-}
-
-fn count_voltage(
-    (joltage, cut_off, pack): (usize, usize, &str),
-    n: usize,
-) -> (usize, usize, &str) {
-    let (index, value) = extract(pack, cut_off, n);
-    (
-        joltage
-            + 10usize.pow(
+    for pack in content.lines() {
+        let mut cut_off = pack.len();
+        let mut joltage = 0;
+        for n in (0..length).rev() {
+            let (index, value) = extract(pack, cut_off, n);
+            cut_off = index + 1;
+            joltage += 10usize.pow(
                 n.try_into()
                     .unwrap(),
-            ) * value,
-        index + 1,
-        pack,
-    )
+            ) * value;
+        }
+
+        counter += joltage;
+    }
+    return counter;
 }
 
 fn extract(pack: &str, take: usize, skip: usize) -> (usize, usize) {
@@ -46,6 +29,8 @@ fn extract(pack: &str, take: usize, skip: usize) -> (usize, usize) {
                 .unwrap() as usize
         })
         .enumerate()
-        .max_by(|(_, a), (_, b)| a.cmp(&b))
+        .max_by(|a, b| {
+            a.1.cmp(&b.1)
+        })
         .unwrap()
 }
